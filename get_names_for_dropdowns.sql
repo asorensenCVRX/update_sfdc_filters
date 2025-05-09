45b86996366203a35f372d96ce7a86ee79301979
asorensen <asorensen@cvrx.com>
2025-05-09 12:45:55 -0600

    dashboard updates

--- get_names_for_dropdowns.sql
+++ get_names_for_dropdowns.sql
@@ -20,27 +20,28 @@ FROM
     LEFT JOIN sfdcUser AS B ON A.REP_EMAIL = B.EMAIL
 WHERE
     NAME <> 'xAndrew xLord'
-UNION
-ALL
-SELECT
-    DISTINCT 'FCE',
-    AM_FOR_CREDIT,
-    b.LNAME_REP,
-    AM_FOR_CREDIT_EMAIL
-FROM
-    qryOpps A
-    LEFT JOIN (
-        SELECT
-            REP_EMAIL,
-            LNAME_REP
-        FROM
-            qryRoster
-        WHERE
-            STATUS = 'ACTIVE'
-            AND ROLE IN('FCE')
-    ) b ON A.AM_FOR_CREDIT_EMAIL = B.REP_EMAIL
-WHERE
-    B.REP_EMAIL IS NOT NULL
-    AND OPP_STATUS <> 'cancelled'
-ORDER BY
-    LNAME_REP
+ORDER BY A.LNAME_REP
+-- UNION
+-- ALL
+-- SELECT
+--     DISTINCT 'FCE',
+--     AM_FOR_CREDIT,
+--     b.LNAME_REP,
+--     AM_FOR_CREDIT_EMAIL
+-- FROM
+--     qryOpps A
+--     LEFT JOIN (
+--         SELECT
+--             REP_EMAIL,
+--             LNAME_REP
+--         FROM
+--             qryRoster
+--         WHERE
+--             STATUS = 'ACTIVE'
+--             AND ROLE IN('FCE')
+--     ) b ON A.AM_FOR_CREDIT_EMAIL = B.REP_EMAIL
+-- WHERE
+--     B.REP_EMAIL IS NOT NULL
+--     AND OPP_STATUS <> 'cancelled'
+-- ORDER BY
+--     LNAME_REP
